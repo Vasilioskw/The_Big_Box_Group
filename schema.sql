5ef3c071ea04bff4223793c0752d425ef0df0315
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-05-06 21:56:53 -0400

    started sql-cli. next step is integrating it with js to make postable forum on User-interface

---
+++ schema.sql
@@ -0,0 +1,15 @@
+create table A_posts (
+    id serial primary key,
+    A-area varchar(50) not null,
+    A-building-sqft varchar(50) not null
+    A-land-acerage varchar(50) not null,
+    A-category varchar(50) not null
+);
+
+create table D_posts (
+    id serial primary key,
+    D-area varchar(50) not null,
+    D-building-sqft varchar(50) not null
+    D-land-acerage varchar(50) not null,
+    D-category varchar(50) not null
+);
