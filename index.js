d8732f0eee4ac0e9d9085645c4a2c8a8c6248e4a
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-05-11 20:15:28 -0400

    working on OMR

--- index.js
+++ index.js
@@ -12,12 +12,37 @@ app.set('views', 'templates');
 app.set('view engine', 'html');
 
 const server = http.createServer(app);
-const db = require('./db');
 
-app.get('/aquisitions', (req, res) => {
-    res.render('aquisitions.html');
+app.get('/', (req, res) => {
+
+    res.render('TBBG')
 });
 
+app.get('/profile', (req, res) => {
+
+    res.render('profile')
+});
+
+
+
+app.get('/aquisitions', (req, res) => {
+const aposts = {
+    aarea: "Houston, TX", 
+    abuildingsqft: 40943, 
+    alandacerage: 2, 
+    acategory: "Shopping Center"
+};
+
+    res.render('aquisitions', {
+        locals: {
+            aposts
+        }
+});
+})
 app.get('/dispositions', (req, res) => {
-    res.render('/dispositions.html');
+    res.render('/dispositions');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}`)
 });
