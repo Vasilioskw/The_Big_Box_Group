d4297c57527353d31fd623f3bb3ae38af502c7ea
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-06-18 22:53:45 -0400

    spelling check

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ app.use(express.urlencoded({
     extended: false
 }))
 
+
+var http = require('http')
+var favicon = require('serve-favicon')
+var finalhandler = require('finalhandler')
+var path = require('path')
+
+var _favicon = favicon(path.join(__dirname, 'public', 'favicon.ico'))
+
+var server = http.createServer(function onRequest (req, res) {
+  var done = finalhandler(req, res)
+
+  _favicon(req, res, function onNext (err) {
+    if (err) return done(err)
+
+    // continue to process the request here, etc.
+
+    res.statusCode = 404
+    res.end('oops')
+  })
+})
+
+
 const {apost, dpost} = require("./models");
 
 app.get('/', (req, res) => {
